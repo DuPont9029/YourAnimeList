af4c85e491fd664f9de1df82bba05917aa117b2d
DuPont9029 <112567672+DuPont9029@users.noreply.github.com>
2023-07-02 18:22:42 +0200

    feat: :sparkles:

--- src/popup.js
+++ src/popup.js
@@ -1,3 +1,5 @@
+
+
 function showCustomPopup() {
     var popup = document.getElementById('custom-popup');
     popup.style.display = 'block';
@@ -13,6 +15,11 @@ function handleConfirmation(choice) {
         // L'utente ha confermato di aver già scaricato il file XML
         // Continua l'elaborazione
         // ...
+
+        // Crea o aggiorna il cookie con una durata di 5 ore
+        var now = new Date();
+        var expiry = new Date(now.getTime() + 5 * 60 * 60 * 1000); // 5 ore in millisecondi
+        document.cookie = 'myCookie=1; expires=' + expiry.toUTCString() + '; path=/';
     } else {
         window.location.href = "http://myanimelist.net/panel.php?go=export";
     }
@@ -20,8 +27,25 @@ function handleConfirmation(choice) {
     closePopup();
 }
 
+function checkCookie() {
+    var cookies = document.cookie.split(';');
+    var found = false;
+
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i].trim();
+        if (cookie.indexOf('myCookie=') === 0) {
+            found = true;
+            break;
+        }
+    }
+
+    return found;
+}
 
 document.addEventListener('DOMContentLoaded', function() {
-    showCustomPopup();
-});
+    var cookieExists = checkCookie();
 
+    if (!cookieExists) {
+        showCustomPopup();
+    }
+});
